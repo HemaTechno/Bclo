c7bb0adaf29c7998f43f58d6907340256ee21330
HemaTechno <ibrahimmansour4655@gmail.com>
2025-06-02 22:58:29 +0300

    Update firebase-config.js

--- db/firebase-config.js
+++ db/firebase-config.js
@@ -1,12 +1,13 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDhpIG2co2LIV9m_E1a5LE6fNqbceI_eCw",
   authDomain: "cloud-c5b93.firebaseapp.com",
   projectId: "cloud-c5b93",
-  storageBucket: "cloud-c5b93.firebasestorage.app",
+  storageBucket: "cloud-c5b93.appspot.com",  // صححت الرابط هنا
   messagingSenderId: "96719477673",
   appId: "1:96719477673:web:11dc6c42f36f139a0782b8",
   measurementId: "G-LSQ0249S3D"
@@ -16,4 +17,5 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 const db = getFirestore(app);
-export { auth, provider, db};
+
+export { auth, provider, db };
